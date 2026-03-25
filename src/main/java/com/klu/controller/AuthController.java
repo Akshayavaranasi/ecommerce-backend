52e7043a2bfbb5136284849533d290840e818214
Akshayavaranasi <2400090222@kluniversity.in>
2026-03-25 23:45:05 +0530

    Update AuthController.java

--- src/main/java/com/klu/controller/AuthController.java
+++ src/main/java/com/klu/controller/AuthController.java
@@ -7,24 +7,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "*")
-//@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final UserService userService;
 
+    // Constructor Injection
     public AuthController(UserService userService) {
         this.userService = userService;
     }
 
-    // Signup
+    // Signup API
     @PostMapping("/signup")
     public User register(@RequestBody User user) {
         return userService.registerUser(user);
     }
 
-    // Login
+    // Login API
     @PostMapping("/login")
-public User login(@RequestBody User user) {
-    return userService.login(user.getEmail(), user.getPassword());
-}
+    public User login(@RequestBody User user) {
+        return userService.login(user.getEmail(), user.getPassword());
+    }
 }
